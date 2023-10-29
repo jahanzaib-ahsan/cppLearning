6a8aa7e66041fc9d507e8ff8921d416d2dd718ab
jahanzaib-ahsan <mail.jahanzaibahsan@gmail.com>
2023-10-30 00:19:13 +0500

    first commit

---
+++ EvenOdd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int number;
+
+    cout<<"Enter a number"<<endl;
+    cin>>number;
+    if(number%2 == 0)
+    {
+    cout<<"The number "<< number<<  " entered is even"<<endl;
+    }
+    else if (number%2 !=0)
+    {
+      cout<<"The number" <<number <<"entered is odd"<<endl;     
+    }
+    return 0;
+}
